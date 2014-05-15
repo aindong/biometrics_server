3501106c5c68c2e71eef43ae4a2caf15dc62e87a
denezra <den.segunial@gmail.com>
2014-05-15 09:39:17 -0700

    Added Folder Entity
    Device Communication

---
+++ biometrics_server/biometrics_server/Entity/DeviceCom.cs
@@ -0,0 +1,18 @@
+﻿using Riss.Devices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biometrics_server.Entity
+{
+    class DeviceCom
+    {
+        public DeviceCom() { }
+
+        public DeviceConnection DeviceConnection() { get; set; }
+
+        public Device Device() { get; set; }
+    }
+}
