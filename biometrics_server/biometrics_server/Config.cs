72326995ea5434b6e0f18a29730ab3cbc628431d
papabear550 <freedom_war550@live.com.ph>
2014-05-15 09:06:47 +0800

    created a configuration class

---
+++ biometrics_server/biometrics_server/Config.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biometrics_server
+{
+    class Config
+    {
+        public static string DB_Host;
+        public static string DB_Name;
+        public static string DB_User;
+        public static string DB_Pass;
+
+        public static string ConnectionString = string.Format("Server={0};Database={1};Uid={2};Pwd={3};", DB_Host, DB_Name, DB_User, DB_Pass);
+
+
+    }
+}
