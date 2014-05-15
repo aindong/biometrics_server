0b15c4740fec008b44461b3bdf45570a685f0471
nkcheng123 <nkmcheng@live.com>
2014-05-15 10:12:50 +0800

    changed the password char

--- biometrics_server/biometrics_server/frmConnect.Designer.cs
+++ biometrics_server/biometrics_server/frmConnect.Designer.cs
@@ -99,6 +99,7 @@
             // 
             this.txtPassword.Location = new System.Drawing.Point(86, 168);
             this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(172, 20);
             this.txtPassword.TabIndex = 13;
             // 
