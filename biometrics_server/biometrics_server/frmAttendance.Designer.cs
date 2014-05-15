115905b867acddc3dc79e883730db7cbd256d7d9
denezra <den.segunial@gmail.com>
2014-05-15 10:12:02 -0700

    Added ListView in the Attendance Form

--- biometrics_server/biometrics_server/frmAttendance.Designer.cs
+++ biometrics_server/biometrics_server/frmAttendance.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.label4 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Employee = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Date = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -52,11 +56,36 @@
             this.panel2.Size = new System.Drawing.Size(694, 45);
             this.panel2.TabIndex = 10;
             // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Employee,
+            this.Date,
+            this.Time});
+            this.listView1.Location = new System.Drawing.Point(14, 81);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(694, 97);
+            this.listView1.TabIndex = 11;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Employee
+            // 
+            this.Employee.Text = "Employee";
+            // 
+            // Date
+            // 
+            this.Date.Text = "Date";
+            // 
+            // Time
+            // 
+            this.Time.Text = "Time";
+            // 
             // frmAttendance
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(722, 407);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.panel2);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -74,5 +103,9 @@
 
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Employee;
+        private System.Windows.Forms.ColumnHeader Date;
+        private System.Windows.Forms.ColumnHeader Time;
     }
 }
