67cdee8dfd247faf3987e8d43316a3c8e599d120
papabear550 <freedom_war550@live.com.ph>
2014-05-15 08:54:23 +0800

    added mysql.data connector

--- biometrics_server/biometrics_server/frmMain.Designer.cs
+++ biometrics_server/biometrics_server/frmMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace biometrics_server
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(427, 226);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Biometrics Server";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
