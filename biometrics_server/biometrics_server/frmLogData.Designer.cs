5a00fa3f3c81bfc8b1a796508e3ba6e619d04599
destromon <jestadena@yahoo.com>
2014-05-19 14:09:08 +0800

    Updated Log Data From Biometrics

--- biometrics_server/biometrics_server/frmLogData.Designer.cs
+++ biometrics_server/biometrics_server/frmLogData.Designer.cs
@@ -28,6 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dtp_End = new System.Windows.Forms.DateTimePicker();
+            this.dtp_Begin = new System.Windows.Forms.DateTimePicker();
+            this.btn_DownloadHistory = new System.Windows.Forms.Button();
+            this.btn_Clear = new System.Windows.Forms.Button();
             this.lvw_GLogList = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -35,15 +39,45 @@
             this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.chk_NewFlag = new System.Windows.Forms.CheckBox();
-            this.dtp_End = new System.Windows.Forms.DateTimePicker();
-            this.dtp_Begin = new System.Windows.Forms.DateTimePicker();
-            this.btn_DownloadNew = new System.Windows.Forms.Button();
-            this.btn_DownloadHistory = new System.Windows.Forms.Button();
-            this.btn_Clear = new System.Windows.Forms.Button();
             this.lbl_EndDate = new System.Windows.Forms.Label();
             this.lbl_BeginDate = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // dtp_End
+            // 
+            this.dtp_End.CustomFormat = "yyyy-MM-dd";
+            this.dtp_End.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtp_End.Location = new System.Drawing.Point(268, 402);
+            this.dtp_End.Name = "dtp_End";
+            this.dtp_End.Size = new System.Drawing.Size(120, 23);
+            this.dtp_End.TabIndex = 20;
+            // 
+            // dtp_Begin
+            // 
+            this.dtp_Begin.CustomFormat = "yyyy-MM-dd";
+            this.dtp_Begin.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtp_Begin.Location = new System.Drawing.Point(53, 402);
+            this.dtp_Begin.Name = "dtp_Begin";
+            this.dtp_Begin.Size = new System.Drawing.Size(120, 23);
+            this.dtp_Begin.TabIndex = 21;
+            // 
+            // btn_DownloadHistory
+            // 
+            this.btn_DownloadHistory.Location = new System.Drawing.Point(441, 433);
+            this.btn_DownloadHistory.Name = "btn_DownloadHistory";
+            this.btn_DownloadHistory.Size = new System.Drawing.Size(103, 25);
+            this.btn_DownloadHistory.TabIndex = 18;
+            this.btn_DownloadHistory.Text = "Get All Glog";
+            this.btn_DownloadHistory.UseVisualStyleBackColor = true;
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Location = new System.Drawing.Point(550, 433);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(99, 25);
+            this.btn_Clear.TabIndex = 19;
+            this.btn_Clear.Text = "Clear All Glog";
+            this.btn_Clear.UseVisualStyleBackColor = true;
             // 
             // lvw_GLogList
             // 
@@ -57,11 +91,11 @@
             this.lvw_GLogList.FullRowSelect = true;
             this.lvw_GLogList.GridLines = true;
             this.lvw_GLogList.HideSelection = false;
-            this.lvw_GLogList.Location = new System.Drawing.Point(14, 28);
+            this.lvw_GLogList.Location = new System.Drawing.Point(12, 12);
             this.lvw_GLogList.MultiSelect = false;
             this.lvw_GLogList.Name = "lvw_GLogList";
             this.lvw_GLogList.Size = new System.Drawing.Size(637, 383);
-            this.lvw_GLogList.TabIndex = 27;
+            this.lvw_GLogList.TabIndex = 17;
             this.lvw_GLogList.UseCompatibleStateImageBehavior = false;
             this.lvw_GLogList.View = System.Windows.Forms.View.Details;
             // 
@@ -99,80 +133,23 @@
             this.columnHeader6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.columnHeader6.Width = 150;
             // 
-            // chk_NewFlag
-            // 
-            this.chk_NewFlag.AutoSize = true;
-            this.chk_NewFlag.Location = new System.Drawing.Point(461, 422);
-            this.chk_NewFlag.Name = "chk_NewFlag";
-            this.chk_NewFlag.Size = new System.Drawing.Size(154, 19);
-            this.chk_NewFlag.TabIndex = 35;
-            this.chk_NewFlag.Text = "Clear New Glog Position";
-            this.chk_NewFlag.UseVisualStyleBackColor = true;
-            // 
-            // dtp_End
-            // 
-            this.dtp_End.CustomFormat = "yyyy-MM-dd";
-            this.dtp_End.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtp_End.Location = new System.Drawing.Point(273, 417);
-            this.dtp_End.Name = "dtp_End";
-            this.dtp_End.Size = new System.Drawing.Size(120, 23);
-            this.dtp_End.TabIndex = 33;
-            // 
-            // dtp_Begin
-            // 
-            this.dtp_Begin.CustomFormat = "yyyy-MM-dd";
-            this.dtp_Begin.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtp_Begin.Location = new System.Drawing.Point(58, 417);
-            this.dtp_Begin.Name = "dtp_Begin";
-            this.dtp_Begin.Size = new System.Drawing.Size(120, 23);
-            this.dtp_Begin.TabIndex = 34;
-            // 
-            // btn_DownloadNew
-            // 
-            this.btn_DownloadNew.Location = new System.Drawing.Point(337, 448);
-            this.btn_DownloadNew.Name = "btn_DownloadNew";
-            this.btn_DownloadNew.Size = new System.Drawing.Size(103, 25);
-            this.btn_DownloadNew.TabIndex = 30;
-            this.btn_DownloadNew.Text = "Get New Glog";
-            this.btn_DownloadNew.UseVisualStyleBackColor = true;
-            this.btn_DownloadNew.Click += new System.EventHandler(this.btn_DownloadNew_Click);
-            // 
-            // btn_DownloadHistory
-            // 
-            this.btn_DownloadHistory.Location = new System.Drawing.Point(446, 448);
-            this.btn_DownloadHistory.Name = "btn_DownloadHistory";
-            this.btn_DownloadHistory.Size = new System.Drawing.Size(103, 25);
-            this.btn_DownloadHistory.TabIndex = 31;
-            this.btn_DownloadHistory.Text = "Get All Glog";
-            this.btn_DownloadHistory.UseVisualStyleBackColor = true;
-            this.btn_DownloadHistory.Click += new System.EventHandler(this.btn_DownloadHistory_Click_1);
-            // 
-            // btn_Clear
-            // 
-            this.btn_Clear.Location = new System.Drawing.Point(555, 448);
-            this.btn_Clear.Name = "btn_Clear";
-            this.btn_Clear.Size = new System.Drawing.Size(99, 25);
-            this.btn_Clear.TabIndex = 32;
-            this.btn_Clear.Text = "Clear All Glog";
-            this.btn_Clear.UseVisualStyleBackColor = true;
-            // 
             // lbl_EndDate
             // 
             this.lbl_EndDate.AutoSize = true;
-            this.lbl_EndDate.Location = new System.Drawing.Point(244, 424);
+            this.lbl_EndDate.Location = new System.Drawing.Point(236, 410);
             this.lbl_EndDate.Name = "lbl_EndDate";
             this.lbl_EndDate.Size = new System.Drawing.Size(24, 15);
-            this.lbl_EndDate.TabIndex = 29;
+            this.lbl_EndDate.TabIndex = 23;
             this.lbl_EndDate.Text = "To:";
             this.lbl_EndDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // lbl_BeginDate
             // 
             this.lbl_BeginDate.AutoSize = true;
-            this.lbl_BeginDate.Location = new System.Drawing.Point(17, 424);
+            this.lbl_BeginDate.Location = new System.Drawing.Point(9, 410);
             this.lbl_BeginDate.Name = "lbl_BeginDate";
             this.lbl_BeginDate.Size = new System.Drawing.Size(38, 15);
-            this.lbl_BeginDate.TabIndex = 28;
+            this.lbl_BeginDate.TabIndex = 22;
             this.lbl_BeginDate.Text = "From:";
             this.lbl_BeginDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -180,19 +157,16 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(785, 522);
-            this.Controls.Add(this.chk_NewFlag);
-            this.Controls.Add(this.dtp_End);
-            this.Controls.Add(this.dtp_Begin);
-            this.Controls.Add(this.btn_DownloadNew);
-            this.Controls.Add(this.btn_DownloadHistory);
-            this.Controls.Add(this.btn_Clear);
+            this.ClientSize = new System.Drawing.Size(658, 480);
             this.Controls.Add(this.lbl_EndDate);
             this.Controls.Add(this.lbl_BeginDate);
+            this.Controls.Add(this.dtp_End);
+            this.Controls.Add(this.dtp_Begin);
+            this.Controls.Add(this.btn_DownloadHistory);
+            this.Controls.Add(this.btn_Clear);
             this.Controls.Add(this.lvw_GLogList);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "frmLogData";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmLogData";
             this.Load += new System.EventHandler(this.frmLogData_Load);
             this.ResumeLayout(false);
@@ -202,6 +176,10 @@
 
         #endregion
 
+        private System.Windows.Forms.DateTimePicker dtp_End;
+        private System.Windows.Forms.DateTimePicker dtp_Begin;
+        private System.Windows.Forms.Button btn_DownloadHistory;
+        private System.Windows.Forms.Button btn_Clear;
         private System.Windows.Forms.ListView lvw_GLogList;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
@@ -209,12 +187,6 @@
         private System.Windows.Forms.ColumnHeader columnHeader4;
         private System.Windows.Forms.ColumnHeader columnHeader5;
         private System.Windows.Forms.ColumnHeader columnHeader6;
-        private System.Windows.Forms.CheckBox chk_NewFlag;
-        private System.Windows.Forms.DateTimePicker dtp_End;
-        private System.Windows.Forms.DateTimePicker dtp_Begin;
-        private System.Windows.Forms.Button btn_DownloadNew;
-        private System.Windows.Forms.Button btn_DownloadHistory;
-        private System.Windows.Forms.Button btn_Clear;
         private System.Windows.Forms.Label lbl_EndDate;
         private System.Windows.Forms.Label lbl_BeginDate;
     }
--- biometrics_server/biometrics_server/frmLogData.cs
+++ biometrics_server/biometrics_server/frmLogData.cs
@@ -7,160 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Riss.Devices;
 using biometrics_server.Entity;
 using biometrics_server.IConvert;
-using Riss.Devices;
 
 namespace biometrics_server
 {
     public partial class frmLogData : Form
     {
-        private Device device;
-        private DeviceConnection deviceConnection;
-        private DeviceComEty deviceEty;
-        public frmLogData(DeviceComEty deviceEty)
+        public frmLogData()
         {
             InitializeComponent();
-            device = deviceEty.Device;
-            deviceConnection = deviceEty.DeviceConnection;
-       
-        }
-        private List<DateTime> GetDateTimeList() {
-            List<DateTime> dtList = new List<DateTime>();
-            dtList.Add(dtp_Begin.Value);
-            dtList.Add(dtp_End.Value);
-            return dtList;
-        }
-
-        private void AddRecordToListView(List<Record> recordList) {
-            lvw_GLogList.Items.Clear();
-            int no = 1;
-            foreach (Record record in recordList) {
-                string type = ConvertObject.GLogType(record.Verify);
-                string mode = ConvertObject.IOMode(record.Action);
-                ListViewItem item = new ListViewItem(new string[]{ no.ToString(), record.DN.ToString(), record.DIN.ToString(),
-                    type, mode, record.Clock.ToString("yyyy-MM-dd HH:mm:ss") });
-                lvw_GLogList.Items.Add(item);
-                no++;
-            }
         }
 
         private void frmLogData_Load(object sender, EventArgs e)
         {
-            dtp_Begin.MinDate = InitData.MinDateTime;
-            dtp_Begin.MaxDate = InitData.MaxDateTime;
-            dtp_Begin.Value = InitData.MinDateTime;
-            dtp_End.MinDate = InitData.MinDateTime;
-            dtp_End.MaxDate = InitData.MaxDateTime;
-            dtp_End.Value = InitData.MaxDateTime;
-        }
 
-        private void btn_DownloadHistory_Click_1(object sender, EventArgs e)
-        {
-            object extraProperty = new object();
-            object extraData = new object();
-            extraData = Global.DeviceBusy;
-
-            try
-            {
-                List<DateTime> dtList = GetDateTimeList();
-                bool result = deviceConnection.SetProperty(DeviceProperty.Enable, extraProperty, device, extraData);
-                extraProperty = false;
-                extraData = dtList;
-                result = deviceConnection.GetProperty(DeviceProperty.AttRecordsCount, extraProperty, ref device,
-                    ref extraData);
-                if (false == result)
-                {
-                    MessageBox.Show("Get All Glog Fail", "Prompt", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                    return;
-                }
-
-                int recordCount = (int)extraData;
-                if (0 == recordCount)
-                {//为0时说明没有新日志记录
-                    lvw_GLogList.Items.Clear();
-                    return;
-                }
-
-                List<bool> boolList = new List<bool>();
-                boolList.Add(false);//所有日志
-                boolList.Add(chk_NewFlag.Checked);//清除新日志标记，false则不清除
-                extraProperty = boolList;
-                extraData = dtList;
-                result = deviceConnection.GetProperty(DeviceProperty.AttRecords, extraProperty, ref device, ref extraData);
-                if (result)
-                {
-                    List<Record> recordList = (List<Record>)extraData;
-                    AddRecordToListView(recordList);
-                }
-                else
-                {
-                    MessageBox.Show("Get All Glog Fail", "Prompt", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            finally
-            {
-                extraData = Global.DeviceIdle;
-                deviceConnection.SetProperty(DeviceProperty.Enable, extraProperty, device, extraData);
-            }
-        }
-
-        private void btn_DownloadNew_Click(object sender, EventArgs e)
-        {
-            object extraProperty = new object();
-            object extraData = new object();
-            extraData = Global.DeviceBusy;
-
-            try
-            {
-                List<DateTime> dtList = GetDateTimeList();
-                bool result = deviceConnection.SetProperty(DeviceProperty.Enable, extraProperty, device, extraData);
-                extraProperty = true;
-                extraData = dtList;
-                result = deviceConnection.GetProperty(DeviceProperty.AttRecordsCount, extraProperty, ref device,
-                    ref extraData);
-                if (false == result)
-                {
-                    MessageBox.Show("Get New Glog Fail", "Prompt", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                    return;
-                }
-
-                int recordCount = (int)extraData;
-                if (0 == recordCount)
-                {//为0时说明没有新日志记录
-                    lvw_GLogList.Items.Clear();
-                    return;
-                }
-
-                List<bool> boolList = new List<bool>();
-                boolList.Add(true);//新日志，该参数为true时，第二个参数清除新日志标记值为true或false时都将强制清除新日志标记
-                boolList.Add(chk_NewFlag.Checked);//清除新日志标记
-                extraProperty = boolList;
-                extraData = dtList;
-                result = deviceConnection.GetProperty(DeviceProperty.AttRecords, extraProperty, ref device, ref extraData);
-                if (result)
-                {
-                    List<Record> recordList = (List<Record>)extraData;
-                    AddRecordToListView(recordList);
-                }
-                else
-                {
-                    MessageBox.Show("Get New Glog Fail", "Prompt", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            finally
-            {
-                extraData = Global.DeviceIdle;
-                deviceConnection.SetProperty(DeviceProperty.Enable, extraProperty, device, extraData);
-            }
         }
     }
 }
