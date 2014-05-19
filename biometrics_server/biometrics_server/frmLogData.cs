314d70d49c10cecd4d5fed5ad00fa696b18bf1f9
destromon <jestadena@yahoo.com>
2014-05-19 14:13:24 +0800

    Updated Log Data v2

--- biometrics_server/biometrics_server/frmLogData.cs
+++ biometrics_server/biometrics_server/frmLogData.cs
@@ -15,14 +15,126 @@ namespace biometrics_server
 {
     public partial class frmLogData : Form
     {
+        private Device device;
+        private DeviceConnection deviceConnection;
+        private DeviceComEty deviceEty;
         public frmLogData()
         {
             InitializeComponent();
+            device = deviceEty.Device;
+            deviceConnection = deviceEty.DeviceConnection;
         }
 
+        private List<DateTime> GetDateTimeList()
+        {
+            List<DateTime> dtList = new List<DateTime>();
+            dtList.Add(dtp_Begin.Value);
+            dtList.Add(dtp_End.Value);
+            return dtList;
+        }
+
+        private void AddRecordToListView(List<Record> recordList)
+        {
+            lvw_GLogList.Items.Clear();
+            int no = 1;
+            foreach (Record record in recordList)
+            {
+                string type = ConvertObject.GLogType(record.Verify);
+                string mode = ConvertObject.IOMode(record.Action);
+                ListViewItem item = new ListViewItem(new string[]{ no.ToString(), record.DN.ToString(), record.DIN.ToString(),
+                    type, mode, record.Clock.ToString("yyyy-MM-dd HH:mm:ss") });
+                lvw_GLogList.Items.Add(item);
+                no++;
+            }
+        }
         private void frmLogData_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btn_DownloadHistory_Click(object sender, EventArgs e)
+        {
+            object extraProperty = new object();
+            object extraData = new object();
+            extraData = Global.DeviceBusy;
+
+            try
+            {
+                List<DateTime> dtList = GetDateTimeList();
+                bool result = deviceConnection.SetProperty(DeviceProperty.Enable, extraProperty, device, extraData);
+                extraProperty = false;
+                extraData = dtList;
+                result = deviceConnection.GetProperty(DeviceProperty.AttRecordsCount, extraProperty, ref device,
+                    ref extraData);
+                if (false == result)
+                {
+                    MessageBox.Show("Get All Glog Fail", "Prompt", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                int recordCount = (int)extraData;
+                if (0 == recordCount)
+                {//为0时说明没有新日志记录
+                    lvw_GLogList.Items.Clear();
+                    return;
+                }
+
+                List<bool> boolList = new List<bool>();
+                boolList.Add(false);//所有日志
+                boolList.Add(chk_NewFlag.Checked);//清除新日志标记，false则不清除
+                extraProperty = boolList;
+                extraData = dtList;
+                result = deviceConnection.GetProperty(DeviceProperty.AttRecords, extraProperty, ref device, ref extraData);
+                if (result)
+                {
+                    List<Record> recordList = (List<Record>)extraData;
+                    AddRecordToListView(recordList);
+                }
+                else
+                {
+                    MessageBox.Show("Get All Glog Fail", "Prompt", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                extraData = Global.DeviceIdle;
+                deviceConnection.SetProperty(DeviceProperty.Enable, extraProperty, device, extraData);
+            }
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            object extraProperty = new object();
+            object extraData = new object();
+            extraData = Global.DeviceBusy;
+
+            try
+            {
+                bool result = deviceConnection.SetProperty(DeviceProperty.Enable, extraProperty, device, extraData);
+                result = deviceConnection.SetProperty(DeviceProperty.AttRecords, extraProperty, device, extraData);
+                if (result)
+                {
+                    lvw_GLogList.Items.Clear();
+                    MessageBox.Show("Clear All Glog Success", "Prompt", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Clear All Glog Fail", "Prompt", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                extraData = Global.DeviceIdle;
+                deviceConnection.SetProperty(DeviceProperty.Enable, extraProperty, device, extraData);
+            }
         }
     }
 }
